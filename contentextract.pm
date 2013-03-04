f13a7ed949520ea3060319bf1633c2031314db64
Yokoda_Toshiaki <adokoy001@gmail.com>
2013-03-04 09:48:49 +0900

    Update contentextract.pm
    add comment.

--- contentextract.pm
+++ contentextract.pm
@@ -2,13 +2,15 @@ package contentextract;
 use strict;
 use LWP::UserAgent;
 use Crypt::SSLeay;
-use HTTP::Cookies;
+use HTTP::Cookies; # for HTTP-Cookies simulation.
 use HTTP::Request::Common qw(POST);
 
+# content extraction from target_URL. GET command
 sub ExtractContent{
   my ($tgturl) = @_;
   my ($ua, $req, $res, $cont);
   my $timename = (localtime(time()))[3];
+  # cookie simulated.
   my $cookie_file = './cookies/cookiefile'.$timename.'.txt';
   my $cookie_jar = HTTP::Cookies->new(file => $cookie_file, autosave => 1, ignore_discard => 1);
   $ua = LWP::UserAgent->new;
@@ -24,11 +26,14 @@ sub ExtractContent{
   return(\$cont);
 }
 
+# content extraction from target_URL by using POST parameter-values set.
 sub ExtractContent_POST{
+  # target_URL and form-values (hash ref).
   my ($tgturl,$form_addr) = @_;
   my %FORM = %$form_addr;
   my ($ua, $req, $res, $cont);
   my $timename = (localtime(time()))[3];
+  # cookie simulated.
   my $cookie_file = './cookies/cookiefile'.$timename.'.txt';
   my $cookie_jar = HTTP::Cookies->new(file => $cookie_file, autosave => 1, ignore_discard => 1);
   $ua = LWP::UserAgent->new;
